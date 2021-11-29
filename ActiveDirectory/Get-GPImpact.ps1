62a5c84aad5d04c283f373502bfeb20d47a9592d
Adrian <adrian.deller@unibas.ch>
2021-11-29 17:04:23 +0100

    Update Get-GPImpact.ps1

--- ActiveDirectory/Get-GPImpact.ps1
+++ ActiveDirectory/Get-GPImpact.ps1
@@ -1,88 +1,148 @@
-﻿Function Get-GPImpact
+function Get-GPImpact
 {
-    [cmdletbinding()]
-    Param(
-        [Parameter(ParameterSetName="Name",Mandatory="True")]
-        [string]$Name
-        ,
-        [Parameter(ParameterSetName="ID",Mandatory="True")]
-        [string]$ID
+    [CmdletBinding()]
+
+    Param
+    (
+        [Parameter(ParameterSetName = 'Name', Mandatory)]
+        [string]
+        $Name,
+
+        [Parameter(ParameterSetName = 'ID', Mandatory)]
+        [string]
+        $ID
     )
-    Begin{}
-    Process{
-        Switch ($psCmdlet.ParameterSetName){
-            "Name" {
-                Write-Verbose "using name"
-                Try{
+
+    Begin
+    {
+
+    }
+    Process
+    {
+        Switch ($PSCmdlet.ParameterSetName)
+        {
+            'Name'
+            {
+                Write-Verbose 'using name'
+                Try
+                {
                     $gpo = Get-GPO -Name $Name
-                }Catch{
+                }
+                Catch
+                {
                     Write-Error $error[0]
                     Return
                 }
-                $scope = Get-GPPermission -All -Name $Name | ?{$_.Permission -eq "GpoApply"}
+                $scope = Get-GPPermission -All -Name $Name | Where-Object { $_.Permission -eq 'GpoApply' }
                 Write-Verbose "Scope: $($scope.trustee.name)"
             }
-            "ID" {
-                Write-Verbose "using id"
-                Try{
+            'ID'
+            {
+                Write-Verbose 'using id'
+                Try
+                {
                     $gpo = Get-GPO -Id $ID
-                }Catch{
+                }
+                Catch
+                {
                     Write-Error $error[0]
                     Return
                 }
-                $scope = Get-GPPermission -All -Id $ID | ?{$_.Permission -eq "GpoApply"}
+                $scope = Get-GPPermission -All -Id $ID | Where-Object { $_.Permission -eq 'GpoApply' }
                 Write-Verbose "Scope: $($scope.trustee.name)"
             }
         }
-        $ous = Get-ADOrganizationalUnit -Filter * -Properties gplink | Where-Object {$_.gplink -like "*$($gpo.id)*"}
-        If((Get-ADDomain).LinkedGroupPolicyObjects -like "*$($gpo.id)*"){
+
+        $GpoName = $Gpo.DisplayName
+
+        $ResultObbject = @{}
+        $ResultObbject.GpoName = $GpoName
+
+        $ous = Get-ADOrganizationalUnit -Filter * -Properties gplink | Where-Object { $_.gplink -like "*$($gpo.id)*" }
+
+        if ((Get-ADDomain).LinkedGroupPolicyObjects -like "*$($gpo.id)*")
+        {
             $ous += (Get-ADDomain).DistinguishedName
         }
-        If($ous){
-            ForEach($OU in $OUs){
-                Write-Verbose "OU: $(OU.Name)"
+        
+        if ($ous)
+        {
+            foreach ($OU in $OUs)
+            {
+                Write-Verbose "OU: $($OU.Name)"
             }
-            If($scope.Trustee){
-                If($scope.Trustee.Name -contains "Authenticated Users"){
-                    Write-Verbose "Authenticated Users"
-                    ForEach($ou in $ous){
+            if ($scope.Trustee)
+            {
+                if ($scope.Trustee.Name -contains 'Authenticated Users')
+                {
+                    Write-Verbose 'Authenticated Users'
+
+                    foreach ($ou in $ous)
+                    {
                         Write-Verbose $ou.distinguishedname
-                        Get-ADObject -SearchBase $ou.distinguishedname -Filter * | Where-Object {@("user","computer") -contains $_.objectclass} | Select Name,ObjectClass
+
+                        $ResultObbject.AffectedObjects = Get-ADObject -SearchBase $ou.distinguishedname -Filter * |
+                            Where-Object { @('user', 'computer') -contains $_.objectclass } |
+                            Select-Object Name, ObjectClass
                     }
-                }Else{
-                    Write-Verbose "Groups"
-                    $groupMembers = @()
+                }
+                else
+                {
+                    Write-Verbose 'Groups'
+
                     #Get group members within scope
-                    ForEach($Group in ($scope | Where-Object {$_.Trustee.SidType -eq "Group"})){
-                        $groupMembers += Get-ADGroupMember $group.Trustee.Name 
+                    $GroupMembers = foreach ($Group in ($scope | Where-Object { $_.Trustee.SidType -eq 'Group' }))
+                    {
+                        Get-ADGroupMember $Group.Trustee.Name 
                     }
-                    $groupMembers = $groupMembers | Select-Object -Unique
+                    $GroupMembers = $GroupMembers | Select-Object -Unique
+
                     #Find members the GPO applies to
-                    If($groupMembers){
+                    if ($groupMembers)
+                    {
                         Write-Verbose "GroupMember count: $($groupMembers.count)"
-                        Foreach($OU in $OUs){
-                            Get-ADObject -SearchBase $ou.DistinguishedName -Filter * | Where-Object {@("user","computer") -contains $_.objectclass} | Where-Object {$groupMembers.Name -contains $_.name} | Select-Object Name,ObjectClass
+                        $ResultObbject.AffectedObjects = foreach ($OU in $OUs)
+                        {
+                            Get-ADObject -SearchBase $ou.DistinguishedName -Filter * |
+                                Where-Object { @('user', 'computer') -contains $_.objectclass } |
+                                Where-Object { $groupMembers.Name -contains $_.name } |
+                                Select-Object Name, ObjectClass
                         }
                     }
+
                     #Find all users and computers explicitly within scope
-                    Write-Verbose "Users"
-                    ForEach($object in ($scope | Where-Object {@("user","computer") -contains $_.Trustee.SidType})){
+                    Write-Verbose 'Users'
+                    $ResultObbject.AffectedObjects += ForEach ($object in ($scope | Where-Object { @('user', 'computer') -contains $_.Trustee.SidType }))
+                    {
                         Write-Verbose $Object.Trustee.Name
-                        Switch($object.Trustee.SidType){
-                            "User" {$object = Get-ADUser $object.Trustee.Name}
-                            "Computer" {$object = Get-ADComputer $object.Trustee.Name}
+
+                        switch ($object.Trustee.SidType)
+                        {
+                            'User' { $object = Get-ADUser $object.Trustee.Name }
+                            'Computer' { $object = Get-ADComputer $object.Trustee.Name }
                         }
-                        If($OUs.DistinguishedName -contains ($object.distinguishedname -creplace "^[^,]*,","")){
+                        if ($OUs.DistinguishedName -contains ($object.distinguishedname -creplace "^[^,]*,", ""))
+                        {
                             Get-ADObject $object.distinguishedname
+                        }
+                        else {
+                            $object
                         }
                     }
                 }
-            }Else{
-                Write-Error "GPO has no scope."
             }
-        }Else{
-            Write-Host "GPO has no links"
+            else
+            {
+                Write-Error 'GPO has no scope.'
+            }
+        }
+        else
+        {
+            Write-Host "GPO '$GpoName' has no links"
         }
     }
-    End{}
+    End
+    {
+        [PSCustomObject]$ResultObbject
+    }
 }
